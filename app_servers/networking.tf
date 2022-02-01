27d764700ab6f26e8c919e07955c258be1b18573
JesseHoch <51460585+JesseHoch@users.noreply.github.com>
2022-02-01 16:53:11 -0500

    Added comments

--- app_servers/networking.tf
+++ app_servers/networking.tf
@@ -1,14 +1,18 @@
+# Imported Resource Group
 resource "azurerm_resource_group" "guru" {
   #name     = "<RESOURCE_GROUP_NAME>"
   #location = "<LOCATION>"
 }
 
+# Creates Azure Virtual Network
 #resource "azurerm_virtual_network" "guru" {
 #  name                = "guru-network"
 #  address_space       = ["10.0.0.0/16"]
 #  location            = azurerm_resource_group.guru.location
 #  resource_group_name = azurerm_resource_group.guru.name
 #}
+
+# Creates Azure Subnet
 #resource "azurerm_subnet" "guru" {
 #  name                 = "guru-subnet"
 #  resource_group_name  = azurerm_resource_group.guru.name
@@ -16,6 +20,7 @@ resource "azurerm_resource_group" "guru" {
 #  address_prefixes     = ["10.0.2.0/24"]
 #}
 
+# Create Azure Public IP
 #resource "azurerm_public_ip" "guru" {
 #  name                = "publicIPForLB"
 #  location            = azurerm_resource_group.guru.location
@@ -23,6 +28,7 @@ resource "azurerm_resource_group" "guru" {
 #  allocation_method   = "Static"
 #}
 
+# Create Azure Network Interface (NIC)
 #resource "azurerm_network_interface" "guru" {
 #  count               = 2
 #  name                = "AZ-VM-00-NIC-${count.index}"
