3cb7b14598ee597692942a33ef8b5510c10195ab
JesseHoch <51460585+JesseHoch@users.noreply.github.com>
2022-02-02 16:23:41 -0500

    Update networking.tf

--- app_servers/networking.tf
+++ app_servers/networking.tf
@@ -25,11 +25,9 @@ resource "azurerm_resource_group" "guru" {
 # Creates Azure Subnet
 #resource "azurerm_subnet" "guru-sub" {
 #  name                 = "guru-subnet"
-#  location             = azurerm_resource_group.guru.location
 #  resource_group_name  = azurerm_resource_group.guru.name
 #  virtual_network_name = azurerm_virtual_network.guru.name
 #  address_prefixes     = ["10.0.2.0/24"]
-#  tags                 = var.tags
 #}
 
 # Create Azure Public IP
@@ -38,21 +36,6 @@ resource "azurerm_resource_group" "guru" {
 #  location            = azurerm_resource_group.guru.location
 #  resource_group_name = azurerm_resource_group.guru.name
 #  allocation_method   = "Static"
-#  domain_name_label   = random_string.fqdn.result
+#  domain_name_label   = random_string.guru-fqdn.result
 #  tags                = var.tags
-#}
-
-# Create Azure Network Interface (NIC)
-#resource "azurerm_network_interface" "guru-nic" {
-#  count               = 2
-#  name                = "AZ-VM-00-NIC-${count.index}"
-#  location            = azurerm_resource_group.guru.location
-#  resource_group_name = azurerm_resource_group.guru.name
-
-#  ip_configuration {
-#    name                          = "guru-config"
-#    subnet_id                     = azurerm_subnet.guru.id
-#    private_ip_address_allocation = "Dynamic"
-#  }
-#  tags = var.tags
 #}
