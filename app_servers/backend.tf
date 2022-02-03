1730bcc08bdcb329af6f69a5653e55ce781ef147
JesseHoch <51460585+JesseHoch@users.noreply.github.com>
2022-02-03 12:48:11 -0500

    Update backend.tf

--- app_servers/backend.tf
+++ app_servers/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   # Backend Remote State Storage in Azure Storage
   backend "azurerm" {
-    resource_group_name  = "1-c1578a37-playground-sandbox"
-    storage_account_name = "tfstates2hcr"
+    resource_group_name  = "<resource_group>"
+    storage_account_name = "<storage_account_name>"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
