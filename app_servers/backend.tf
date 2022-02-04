e7f2213aa47f1d39be6a42dbc8334bad035c51ca
JesseHoch <51460585+JesseHoch@users.noreply.github.com>
2022-02-04 11:40:50 -0500

    Update backend.tf

--- app_servers/backend.tf
+++ app_servers/backend.tf
@@ -1,5 +1,6 @@
 terraform {
   # Backend Remote State Storage in Azure Storage
+  # You can not use variables for the values in this resource
   backend "azurerm" {
     resource_group_name  = "<RESOURCE_GROUP_NAME>"
     storage_account_name = "<STORAGE_ACCOUNT_NAME>"
